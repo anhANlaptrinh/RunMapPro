3f4b50c3968fce675228392786919d7342c92cc6
Đỗ Huỳnh An <dohuynhan2408@gmail.com>
2025-10-27 17:27:09 +0700

    Alo

---
+++ backendcode/src/main/java/com/example/backendcode/BackendcodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendcodeApplication.class, args);
+	}
+
+}
---
+++ backendcode/src/test/java/com/example/backendcode/BackendcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
